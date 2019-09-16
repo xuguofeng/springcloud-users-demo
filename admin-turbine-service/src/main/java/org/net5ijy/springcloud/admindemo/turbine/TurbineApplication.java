a97ea94ab5fc8349f10aeb82acdf9965f1dfbb2d
xuguofeng <xuguofeng1990@126.com>
2019-09-16 16:55:08 +0800

    优化turbine代码

--- admin-turbine-service/src/main/java/org/net5ijy/springcloud/admindemo/turbine/TurbineApplication.java
+++ admin-turbine-service/src/main/java/org/net5ijy/springcloud/admindemo/turbine/TurbineApplication.java
@@ -2,6 +2,9 @@ package org.net5ijy.springcloud.admindemo.turbine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 /**
@@ -10,8 +13,11 @@ import org.springframework.cloud.netflix.turbine.EnableTurbine;
  * @author xuguofeng
  * @date 2019/5/29 12:32
  */
+@EnableDiscoveryClient
 @SpringBootApplication
 @EnableTurbine
+@EnableHystrixDashboard
+@EnableHystrix
 public class TurbineApplication {
 
 	public static void main(String[] args) {
